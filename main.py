d76e6d80f3e296ab72b4901ef5cf203fccaa94f7
navarrot <tnavarrobr@gmail.com>
2024-03-12 11:36:35 +0100

    Added prediction workflow to main.py. Added samsung pdf and base_model_3.h5

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data.pdf_proc import crop_pdf, output_ecgs, convert_to_images, remove_grid
 from data.params import smsng_crop_areas
-from models.final_model import model_load_compile, predict
+from models.final_model import model_load_compile
+import tensorflow as tf
+import os
 
 
 # Crop the Samsung ECG
@@ -10,13 +12,23 @@ output_ecgs('samsung_ecg.pdf', smsng_crop_areas)
 for i in range(3):
     convert_to_images(f'ecg{i + 1}.pdf', f'ecg{i + 1}.jpg')
 
-# Remove the gridlines from the images
+# Remove the gridlines from the imagesx
 for i in range(3):
     remove_grid(f'ecg{i + 1}.jpg', f'ecg{i + 1}_no_grid.jpg')
 
 
 # Load the model
-#model_load_compile('base_model_backup.h5')
+model = model_load_compile('models/base_model_3.h5')
+
+#Take one ecg picture to make a prediction
+img = tf.keras.preprocessing.image.load_img('ecg1_no_grid.jpg')
+
+# Resize the image to match the target shape
+new_height, new_width = 79, 622  # Specify the target height and width
+resized_img = tf.image.resize(img, (new_height, new_width))
+
+# Expand the dimensions to create a batch with a single image
+input_img = tf.expand_dims(resized_img/255, axis=0)
 
 # Make predictions
-#predict(model, 'ecg1_no_grid.jpg')
+print(model.predict(input_img))
--- models/final_model.py
+++ models/final_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 # Specify the path to your saved model file (replace 'your_model.h5' with the actual filename)
-model_path = 'base_model_backup.h5'
+#model_path = './base_model_3.h5'
 
 def model_load_compile(model_path):
     # Load the model from the .h5 file
@@ -11,6 +12,8 @@ def model_load_compile(model_path):
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-def predict(model, image_path):
 
-    model.predict(image_path)
+#model = model_load_compile(model_path)
+
+#img = tf.keras.preprocessing.image.load_img('../ecg1.jpg')
+#print(model.predict(plt.imread('../ecg1_no_grid.jpg')))
